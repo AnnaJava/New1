4bd848d2e1e9aaec7cc6c3604abfbe723f241afb
AnnaJava <Anna.Karpycheva@gmail.com>
2016-02-19 18:06:33 +0200

    Второй коммит

---
+++ src/Class1.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Anna Karpycheva on 19.02.2016.
+ */
+public class Class1{
+    int anInt = 1;
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Anna Karpycheva on 19.02.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
